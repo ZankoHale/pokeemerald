1beb3eb1672f50313358b6456bf78ee6305ac8ec
ZankoHale <47875338+ZankoHale@users.noreply.github.com>
2019-08-10 01:17:04 +0100

    MoveTutor teaches egg moves

--- src/move_relearner.c
+++ src/move_relearner.c
@@ -896,12 +896,15 @@ static void RemoveScrollArrows(void)
     }
 }
 
+u8 GetEggMoves(struct Pokemon *pokemon, u16 *eggMoves);
+
 static void CreateLearnableMovesList(void)
 {
     s32 i;
     u8 nickname[POKEMON_NAME_LENGTH + 1];
 
     sMoveRelearnerStruct->numMenuChoices = GetMoveRelearnerMoves(&gPlayerParty[sMoveRelearnerStruct->partyMon], sMoveRelearnerStruct->movesToLearn);
+	sMoveRelearnerStruct->numMenuChoices += GetEggMoves(&gPlayerParty[sMoveRelearnerStruct->partyMon], sMoveRelearnerStruct->movesToLearn + sMoveRelearnerStruct->numMenuChoices);
 
     for (i = 0; i < sMoveRelearnerStruct->numMenuChoices; i++)
     {
