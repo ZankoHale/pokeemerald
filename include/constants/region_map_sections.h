7af4b7a29d6edc75984a1540ca2179251ed59600
ZankoHale <47875338+ZankoHale@users.noreply.github.com>
2019-08-12 23:11:19 +0100

    Revert "mapsec"
    This reverts commit 871fefdc34d632ceefa61e6ac201eeabd958a1d2.

--- include/constants/region_map_sections.h
+++ include/constants/region_map_sections.h
@@ -214,9 +214,9 @@
 #define MAPSEC_ALTERING_CAVE_2              0xD2
 #define MAPSEC_NAVEL_ROCK2                  0xD3
 #define MAPSEC_TRAINER_HILL                 0xD4
-#define MAPSEC_NONE                         0xD5
-#define MAPSEC_DEWFORD_BEACH                0xD6
-#define MAPSEC_CAPE_LILYCOVE                0xD7
+#define MAPSEC_DEWFORD_BEACH                0xD5
+#define MAPSEC_CAPE_LILYCOVE                0xD6
+#define MAPSEC_NONE                         0xD7
 
 #define METLOC_SPECIAL_EGG                  0xFD
 #define METLOC_IN_GAME_TRADE                0xFE
